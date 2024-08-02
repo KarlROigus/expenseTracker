aee86fe9bf00e98bab0ac497cd62cd382b9173b1
KarlROigus <karlrudolfoigus@gmail.com>
2024-08-02 10:11:06 +0300

    Initial view of the page with some of the button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,92 @@
 import { useState } from 'react'
+import { v4 as uuid} from 'uuid';
 import './App.css'
 
 function App() {
 
+  const [expenseList, setExpenseList] = useState([]);
+
+  const [isListVisible, setListVisible] = useState(false);
+  const [isNewExpenseFormVisible, setNewExpenseFormVisible] = useState(false);
+
+  
+
+  function toggleListVisibility() {
+    setListVisible(!isListVisible);
+  }
+
+  function toggleNewExpenseForm() {
+    setNewExpenseFormVisible(!isNewExpenseFormVisible);
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    const newExpense = {expenseType: e.target.expenseType.value, cost: e.target.cost.value, id: uuid()};
+    
+    setExpenseList(currentList => {
+      return [...currentList, newExpense]
+    })
+
+    toggleNewExpenseForm();
+  }
+
   return (
     <>
-      Hi
+      <div className="header">
+        <div>
+          <h1>Expense Tracker</h1>
+        </div>
+      </div>
+      <div className="buttonarea">
+        <button className="expensebutton" onClick={toggleListVisibility}>
+            {isListVisible ? "Hide Expenses" : "Show Expenses"}
+        </button>
+        <button className="expensebutton" onClick={toggleNewExpenseForm}>
+          Add New Expense
+        </button>
+        <button className="expensebutton">
+          Reset All Expenses
+        </button>
+      </div>
+      
+      {isListVisible && 
+            <div className="listarea">
+               <ul>
+                 {expenseList.map(each => <li key={each.id}>Expense Type: {each.expenseType} Cost: {each.cost}</li>)}
+               </ul>
+            </div>
+      }
+
+      {isNewExpenseFormVisible && 
+        <fieldset>
+            <legend>Expense</legend>
+            <form action="#" className="newexpenseform" onSubmit={handleFormSubmit}>
+              <label htmlFor="expenseType">Expense Type: </label>
+              <select name="cars" id="expenseType">
+                <option value=""></option>
+                <option value="utility">Utility</option>
+                <option value="insurance">Insurance</option>
+                <option value="phoneandinternet">Phone And Internet</option>
+                <option value="gym">Gym Membership</option>
+                <option value="subscription">Subscription</option>
+                <option value="groceries">Groceries</option>
+                <option value="eatingout">Eating Out</option>
+                <option value="loan">Loan</option>
+                <option value="clothes">Clothes</option>
+                <option value="skincare">Skin Care</option>
+                <option value="other">Other</option>
+              </select>
+
+              <label htmlFor="cost">Cost: </label>
+              <input type="number" id="cost" step="0.01" min="0.01"/>
+
+              <label htmlFor="date">Date: </label>
+              <input type="date" id="date"/>
+
+              <input type="submit" className="submitBtn"/>
+            </form>
+        </fieldset>
+        }
     </>
   )
 }
